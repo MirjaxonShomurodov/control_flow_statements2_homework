6ba2d616567a10fea8a858daef6920a539744bc9
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-17 14:14:06 +0500

    misol

--- if02.py
+++ if02.py
@@ -1,5 +1,12 @@
 def main(a,b,c):
-    """
+    if a<b and a<c:
+        return a
+    if b<a and b<c:
+        return b
+    if c<a and c<b:
+        return c
+print(main(2,9,7))
+"""
     Find the smallest of the numbers.
     Args:
         a: First number.
@@ -8,4 +15,3 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
