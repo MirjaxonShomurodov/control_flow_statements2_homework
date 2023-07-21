f1a3634e4afef11926ffc83d4aeff15185407b0b
MirjaxonShomurodov <mirjaxon457@gmail.com>
2023-07-21 14:43:52 +0500

    3 misol

--- if03.py
+++ if03.py
@@ -8,4 +8,10 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if (a>b and a<c) or (a<b and a>c):
+        return a
+    if (b>a and b<c) or (b<a and b>c):
+        return b
+    if (c>a and c<b) or (c<b and c>b):
+        return c
+print(main(2,5,9))
